da0851b9e3c66a581a6b1d7c084ecf85429a9268
Marinescu Dragos <142613647+DragosMarinescu19@users.noreply.github.com>
2025-08-25 15:55:40 +0300

    Update init_database.sql

--- init_database.sql
+++ init_database.sql
@@ -1,9 +1,19 @@
-create database DataWarehouse
-use DataWarehouse
+
+if exists (select 1 from sys.databases WHERE name ='DataWarehouse')
+begin
+    alter database DataWarehouse set single_user with rollback immediate;
+    drop database DataWarehouse
+end;
 go
-create schema bronze
+  
+create database DataWarehouse
 go
 
+use DataWarehouse
+go
+  
+create schema bronze
+go
 
 create schema silver
 go
