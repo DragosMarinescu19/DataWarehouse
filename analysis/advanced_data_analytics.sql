cb141115219e6b7a0e002962ca1a1e74c35be61b
Marinescu Dragos <142613647+DragosMarinescu19@users.noreply.github.com>
2025-08-27 12:19:31 +0300

    Update advanced_data_analytics.sql

--- analysis/advanced_data_analytics.sql
+++ analysis/advanced_data_analytics.sql
@@ -78,4 +78,75 @@ from yearly_product_sales
 order by product_name,order_year
 
 
+--Group customers into three segments based on their spending behavior
+-- VIP
+--Regular
+--New
+--And find the total nr of customers by each group
+
+with cte as(
+select 
+c.customer_key,
+sum(f.sales_amount) as total_spendings,
+DATEDIFF(month,min(f.order_date),max(f.order_date)) as lifespan
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON f.customer_key=c.customer_key
+group by c.customer_key
+)
+select 
+customer_behavior,
+count(customer_key) as total_customers
+from(
+	select 
+	customer_key,
+	CASE WHEN lifespan >=12 and total_spendings>5000 THEN 'VIP'
+		 WHEN lifespan >=12 and total_spendings<5000 THEN 'Regular'
+		 ELSE 'New'
+	END as customer_behavior
+	from cte
+	)t 
+group by customer_behavior
+order by total_customers desc
+
+
+--Segment products into cost ranges and count 
+--how many products fall into each sement
+with products_segments as(
+select 
+product_key,
+product_name,
+cost,
+CASE WHEN cost<100 THEN '<100'
+	 WHEN cost between 100 and 500 THEN '100-500'
+	 WHEN cost between 500 and 1000 then '500-1000'
+	 ELSE '>1000'
+END cost_range
+from gold.dim_products
+)
+select 
+cost_range,
+count(product_key) as total_products
+from products_segments
+group by cost_range
+order by total_products desc
+
+
+
+--Which categories contribute the most to our overall sales
+with category_sales as(
+select 
+p.category as category,
+sum(f.sales_amount) as total_sales
+from gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON f.product_key=p.product_key
+group by p.category
+)
+select category,
+total_sales,
+SUM(total_sales) over() as overall_sales,
+CONCAT(ROUND((CAST(total_sales as FLOAT) / SUM(total_sales) over())* 100,2),'%') as percentage_of_total
+from category_sales
+order by total_sales desc
 
