80137022dd0f07621ee715a47401e4fc9d3189a7
Marinescu Dragos <142613647+DragosMarinescu19@users.noreply.github.com>
2025-09-10 12:17:40 +0300

    Update proc_incremental_load_customer.sql

--- incremental_loads/proc_incremental_load_customer.sql
+++ incremental_loads/proc_incremental_load_customer.sql
@@ -1,4 +1,4 @@
-create or alter procedure incremental_load_customer
+create or alter procedure incremental_load_customer --SCD1
 as
 begin
 set nocount on;
@@ -12,29 +12,41 @@ set nocount on;
 	)
 
 	DECLARE @SummaryOfChanges TABLE(change VARCHAR(20));
+	DECLARE @error_message nvarchar(1000);
+	
+	begin try
+		begin transaction
+			merge dim_customer as TARGET
+			using staging_customer as SOURCE
+			ON(TARGET.customer_id= SOURCE.customer_id)
+			--When records are matched, update the records IF THERE IS ANY CHANGE 
+			--We use SCD_1(pentru a mentine istoricul ,adica SCD_2 sau altele, facem insert aici in loc de update)
+			when matched
+			and hashbytes('sha1',upper(TARGET.customer_name+TARGET.city+TARGET.country)) != 
+				hashbytes('sha1',upper(SOURCE.customer_name+SOURCE.city+SOURCE.country))
+			then
+			update set TARGET.customer_name=SOURCE.customer_name,
+					   TARGET.city=SOURCE.city,
+					   TARGET.country=SOURCE.country
+			--When no records are mathed, insert the new records from source to target
+			when not matched by target
+			then insert (customer_id,customer_name,city,country) values (SOURCE.customer_id,SOURCE.customer_name,SOURCE.city,SOURCE.country)
 
-	merge dim_customer as TARGET
-	using staging_customer as SOURCE
-	ON(TARGET.customer_id= SOURCE.customer_id)
-	--When records are matched, update the records IF THERE IS ANY CHANGE 
-	--We use SCD_1(pentru a mentine istoricul ,adica SCD_2 saui altele, facem insert aici in loc de update)
-	when matched
-	and hashbytes('sha1',upper(TARGET.customer_name+TARGET.city+TARGET.country)) != 
-		hashbytes('sha1',upper(SOURCE.customer_name+SOURCE.city+SOURCE.country))
-	then
-	update set TARGET.customer_name=SOURCE.customer_name,
-			   TARGET.city=SOURCE.city,
-			   TARGET.country=SOURCE.country
-	--When no records are mathed, insert the new records from source to target
-	when not matched by target
-	then insert (customer_id,customer_name,city,country) values (SOURCE.customer_id,SOURCE.customer_name,SOURCE.city,SOURCE.country)
+			output $action into @SummaryOfChanges;
+			select change,count(*) as CountPerChange
+			from @SummaryOfChanges group by change;
 
-	output $action into @SummaryOfChanges;
-	select change,count(*) as CountPerChange
-	from @SummaryOfChanges group by change;
+			insert into log_table select 'Incremental load','dim_customer',(select count(*) from @SummaryOfChanges where change = 'INSERT'),
+			(select count(*) from @SummaryOfChanges where change = 'UPDATE')
+		commit transaction
+	end try
 
-	insert into log_table select 'Incremental load','dim_customer',(select count(*) from @SummaryOfChanges where change = 'INSERT'),
-	(select count(*) from @SummaryOfChanges where change = 'UPDATE')
+	begin catch
+		if @@TRANCOUNT>0 rollback
+		set @error_message=ERROR_MESSAGE()
+		insert into log_table select 'Error - Incremental load : '+@error_message,'dim_customer',0,0;
+	end catch
+
 end;
 go
 exec incremental_load_customer
@@ -43,4 +55,6 @@ go
 select * from log_table
 
 select * from dim_customer where customer_id=1
-select * from staging_customer where customer_id=1
+select * from staging_customer  --asta trebuie rulat ca exemplu pt a demonstra
+insert into staging_customer(customer_id,customer_name,city,country) values(1002,'New_customer2','city2','country2');
+update staging_customer set customer_name='New_customer3' where customer_id=1
