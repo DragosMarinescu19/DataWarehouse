bea0cf3b1a63b59d0e000e38f8a04f4f03cb771a
Marinescu Dragos <142613647+DragosMarinescu19@users.noreply.github.com>
2025-09-17 15:32:04 +0300

    Update update_staging_customer.sql

--- incremental_loads/update_staging_customer.sql
+++ incremental_loads/update_staging_customer.sql
@@ -6,31 +6,71 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    UPDATE T
+    DECLARE @CityCountry TABLE (
+        CityID INT IDENTITY(1,1) PRIMARY KEY,
+        CityName NVARCHAR(100),
+        CountryName NVARCHAR(100)
+    );
+
+    INSERT INTO @CityCountry (CityName, CountryName) VALUES
+        ('Bucharest', 'Romania'),
+        ('Cluj-Napoca', 'Romania'),
+        ('Timisoara', 'Romania'),
+        ('Budapest', 'Hungary'),
+        ('Debrecen', 'Hungary'),
+        ('Vienna', 'Austria'),
+        ('Salzburg', 'Austria'),
+        ('Paris', 'France'),
+        ('Lyon', 'France'),
+        ('Berlin', 'Germany'),
+        ('Munich', 'Germany'),
+        ('Madrid', 'Spain'),
+        ('Barcelona', 'Spain');
+
+
+    UPDATE sc
     SET 
-        T.customer_name = CONCAT('New_name_', NEWID()),
-        T.city = CONCAT('New_city_', NEWID()),
-        T.country = CONCAT('New_country_', NEWID())
+        sc.customer_name = CONCAT('New_name_', NEWID()),
+        sc.city = c.CityName,
+        sc.country = c.CountryName
     FROM (
-        SELECT TOP 100 *
+        SELECT TOP 100 customer_id
         FROM staging_customer
         ORDER BY NEWID()
-    ) AS T;
+    ) t
+    JOIN staging_customer sc ON sc.customer_id = t.customer_id
+    CROSS APPLY (
+        SELECT TOP 1 CityName, CountryName
+        FROM @CityCountry
+        ORDER BY NEWID()
+    ) c;
+
 
     DECLARE @max_id INT;
     SELECT @max_id = ISNULL(MAX(customer_id), 0) FROM staging_customer;
 
+    ;WITH Numbers AS (
+        SELECT TOP (10) ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS n
+        FROM master..spt_values
+    ),
+    RandomCity AS (
+        SELECT 
+            n,
+            ABS(CHECKSUM(NEWID())) % (SELECT COUNT(*) FROM @CityCountry) + 1 AS CityID
+        FROM Numbers
+    )
     INSERT INTO staging_customer (customer_id, customer_name, city, country)
-    SELECT top 10
-        ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) + @max_id AS customer_id,
+    SELECT 
+        r.n + @max_id AS customer_id,
         CONCAT('Customer_', NEWID()),
-        CONCAT('City_', NEWID()),
-        CONCAT('Country_', NEWID())
-    FROM sys.all_objects a
-    CROSS JOIN sys.all_objects b
+        c.CityName,
+        c.CountryName
+    FROM RandomCity r
+    JOIN @CityCountry c ON r.CityID = c.CityID;
 
 END;
 GO
+
 exec update_staging_customer
 
 select * from dim_customer_scd2
